866dda57a496665abc4ff08eaf290235425d1b08
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-18 18:38:47 +0200

    passing sample props

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ function App() {
   return (
     <div className="App">
       <h2>Hello world!!!</h2>
-      <Person></Person>
+      <Person name="Mostakimul Karim" profession="student"></Person>
     </div>
   );
 }
 
 // Create a component
-function Person() {
+function Person(props) {
   const personStyle = {
     backgroundColor: 'lightblue',
   };
   return (
     <div style={personStyle}>
-      <h3>Name: Mostakimul Karim</h3>
-      <p>Profession: Student</p>
+      <h3>Name: {props.name}</h3>
+      <p>Profession: {props.profession}</p>
     </div>
   );
 }
