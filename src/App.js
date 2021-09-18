64bcb038ecb98e5985ac59084b5ca1fe8b8ed76b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-18 19:01:35 +0200

    dynamic object component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 
 function App() {
+  const nayoks = [
+    { name: 'Mostakimul', age: 24 },
+    { name: 'Karim', age: 22 },
+    { name: 'Mohit', age: 23 },
+  ];
+
   return (
     <div className="App">
-      <h2>Hello world!!!</h2>
-      <Person name="Mostakimul Karim" profession="student"></Person>
-    </div>
-  );
-}
-
-// Create a component
-function Person(props) {
-  const personStyle = {
-    backgroundColor: 'lightblue',
-  };
-  return (
-    <div style={personStyle}>
-      <h3>Name: {props.name}</h3>
-      <p>Profession: {props.profession}</p>
+      <ul>
+        {nayoks.map((nayok) => (
+          <li>{nayok.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
